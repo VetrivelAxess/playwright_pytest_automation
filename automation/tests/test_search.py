a9839c83fde3a8f824043bade15315c02537ad96
VetrivelAxess <139960916+VetrivelAxess@users.noreply.github.com>
2023-07-20 18:25:26 +0530

    Update test_search.py

--- automation/tests/test_search.py
+++ automation/tests/test_search.py
@@ -1,11 +1,10 @@
-
 def test_mercado_libre_search(MyHomePage):
     MyHomePage.load()
 
     # Assert that the page title contains "e"
     assert "Home - Alphacode" in MyHomePage.page.title(), "The alphacode website is loaded successfully."
 
+
 def test_navigate_to_contact(MyHomePage):
     MyHomePage.load()
     MyHomePage.navigate_to_contact()
-
