09787359ace16ffb32e26176af9147080a95be95
VetrivelAxess <139960916+VetrivelAxess@users.noreply.github.com>
2023-07-26 12:35:37 +0530

    Adding files

--- automation/tests/test_search.py
+++ automation/tests/test_search.py
@@ -1,18 +1,17 @@
 import allure
 
-def test_mercado_libre_search(MyHomePage):
+
+def test_alpha_code_search(MyHomePage):
     with allure.step("Load the home page"):
         MyHomePage.load()
 
     # Assert that the page title contains "e"
     with allure.step("Assert that the page title contains 'e'"):
         assert "Home - Alphacode" in MyHomePage.page.title(), "The alphacode website is loaded successfully."
-    # You can also attach screenshots, HTML, or other data to the report using:
-    # allure.attach('screenshot.png', name='Screenshot', attachment_type=allure.attachment_type.PNG)
+
 
 def test_navigate_to_contact(MyHomePage):
     with allure.step("Load the home page"):
         MyHomePage.load()
     with allure.step("Navigate to the contact page"):
         MyHomePage.navigate_to_contact()
-
